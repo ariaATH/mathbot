7016614dea5756427c39fc0dc96026ebc537c02a
ATH <aria123sparco@gmail.com>
2025-06-04 12:30:00 +0330

    fix bug modifire bug and delet modifiers from many function

--- contracts/ContestPrize.sol
+++ contracts/ContestPrize.sol
@@ -42,8 +42,9 @@ contract ContestPrize is Ownable {
     }
 
     // Divides the prizes of a competition among the winners with predetermined percentages
-    function Awardwinners(address payable _first, address payable _second,address payable _Third,uint _ID ) external CheckSameId(_ID) CheckActive(_ID) onlyOwner  {
+    function Awardwinners(address payable _first, address payable _second,address payable _Third,uint _ID ) external CheckActive(_ID) onlyOwner  {
         require(_first != address(0) && _second != address(0) && _Third != address(0), "Invalid address");
+        require(Components[_ID].exist == true , "invalid ID");
         uint award = Components[_ID].Total_amount;
         _first.transfer((award * 30) / 100);
         _second.transfer((award * 20) / 100);
@@ -58,14 +59,16 @@ contract ContestPrize is Ownable {
     receive() external payable{}
 
     // return total budget of a contest with get ID
-    function getcomptotal(uint _ID) external view CheckSameId(_ID) returns(uint){
-            return Components[_ID].Total_amount;
+    function getcomptotal(uint _ID) external view returns(uint){
+        require(Components[_ID].exist == true , "invalid ID");
+        return Components[_ID].Total_amount;
     }
 
     // Distributes the prizes of a contest according to the percentage of entries.
-    function AwardWithPercentage(address payable _first, address payable _second,address payable _Third , uint percent1 , uint percent2 , uint percent3 , uint _ID)external CheckSameId(_ID) CheckActive(_ID) onlyOwner {
+    function AwardWithPercentage(address payable _first, address payable _second,address payable _Third , uint percent1 , uint percent2 , uint percent3 , uint _ID)external CheckActive(_ID) onlyOwner {
         uint sum = percent1 + percent2 + percent3;
         require(sum <= 100 , "Wrong percentages");
+        require(Components[_ID].exist == true , "invalid ID");
         uint award = Components[_ID].Total_amount;
          _first.transfer((award * percent1) / 100);
         _second.transfer((award * percent2) / 100);
