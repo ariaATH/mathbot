cd35b16e472c4065a6944efba06be6cbab685bd7
Hossein <hosseinakbari506@gmail.com>
2025-06-19 15:56:50 +0330

    Comment out unused team member images in About.js to clean up the code.

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -3,9 +3,9 @@ import { Helmet } from 'react-helmet';
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import Hossein from '../assets/images/hossein-akbari.jpeg';
-import Maziar from '../assets/images/maziar-heidari.jpg';
-import Olia from '../assets/images/olia-abbasi.jpg';
-import MohammadReza from '../assets/images/mohammadreza-taheri.jpg';
+// import Maziar from '../assets/images/maziar-heidari.jpg';
+// import Olia from '../assets/images/olia-abbasi.jpg';
+// import MohammadReza from '../assets/images/mohammadreza-taheri.jpg';
 
 class About extends Component {
     render () {
@@ -21,30 +21,16 @@ class About extends Component {
                 <div className="about-us-section">
                     <div className="about">
                         <h1 className="about-title">تیم ما</h1>
-                            <p className="about-description">اگر علاقه مند به عضویت در تیم ما و کار روی پروژه های اپن سورس دارید با ما ارتباط بگیرید</p>
-                            <div className="row">
-                                <div className="col-md-4 col-sm-4">
-                                    <div className="our-team-box">
-                                        <img src={Hossein} className="our-team-img" alt="Hossein Akbari" />
-                                        <h3>Hossein Akbari</h3>
-                                        <h4>Full-stack Developer</h4>
-                                    </div>
+                        <p className="about-description">اگر علاقه مند به عضویت در تیم ما و کار روی پروژه های اپن سورس دارید با ما ارتباط بگیرید</p>
+                        <div className="row">
+                            <div className="col-md-4 col-sm-4">
+                                <div className="our-team-box">
+                                    <img src={Hossein} className="our-team-img" alt="Hossein Akbari" />
+                                    <h3>Hossein Akbari</h3>
+                                    <h4>Full-stack Developer</h4>
                                 </div>
-                                <div className="col-md-4 col-sm-4">
-                                    <div className="our-team-box">
-                                        <img src={Maziar} className="our-team-img" alt="Maziar Heidari" />
-                                        <h3>Maziar Heidari</h3>
-                                        <h4>Backend Developer</h4>
-                                    </div>
-                                </div>
-                                <div className="col-md-4 col-sm-4">
-                                    <div className="our-team-box">
-                                        <img src={Olia} className="our-team-img" alt="Olia Abbasi" />
-                                        <h3>Olia Abbasi</h3>
-                                        <h4>Logo designer</h4>
-                                    </div>
-                                </div>
-                            </div>                            
+                            </div>
+                        </div>                            
                     </div>
                 </div>
 
