06b4390db64f3f3ede2d507e2615a593594dd1aa
ATH <aria123sparco@gmail.com>
2025-06-28 20:15:42 +0330

    refactor create_Tx function to improve transaction gas limit and ensure JSON output

--- contracts/api/web3_config.py
+++ contracts/api/web3_config.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 from .contract_config import contract_address, contract_abi
 import os
+import json
 from dotenv import load_dotenv
 
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
@@ -11,12 +12,15 @@ address_node = Web3(Web3.HTTPProvider(RPC_URL))
 w3 = Web3(address_node)
 contract = address_node.eth.contract(address=contract_address, abi=contract_abi)
 
+# create a transaction to send ether to the contract from user get user address and component price
 def create_Tx(value , user_address):
-  tx = {
-    'to' : contract_address,
-    'value' : hex(w3.to_wei(value , 'ether')),
-    'gas' :  hex(2100),                     
-    'gasPrice' :hex(w3.eth.gas_price),
-    'nonce' :  w3.eth.get_transaction_count(user_address),
-    'chainId' : 56
-  }
+    tx = {
+      'to' : contract_address,
+      'value' : hex(w3.to_wei(value , 'ether')),
+      'gas' :  hex(100000),                     
+      'gasPrice' :hex(w3.eth.gas_price),
+      'nonce' :  w3.eth.get_transaction_count(user_address),
+      'chainId' : 56
+    }
+    return json.dumps(tx)
+
