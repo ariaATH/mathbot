a1aa7975baeff06722cc3926ec75633f9edf5ef3
ATH <aria123sparco@gmail.com>
2025-11-17 15:20:41 +0330

    write test for awardwithpercentage function

--- contracts/Contest_prize/test/ContestPrize.t.sol
+++ contracts/Contest_prize/test/ContestPrize.t.sol
@@ -56,7 +56,21 @@ contract ContestPrizeTest is Test {
          
    }
 
-   
+   function testawardwithpercentage() public {
+      address _testaddress1 = 0x0000000000000000000000000000000000001234 ;
+      address _testaddress2 = 0x0000000000000000000000000000000000004231 ;
+      address _testaddress3 = 0x0000000000000000000000000000000000005343 ;
+      vm.deal(_testaddress1, 0);
+      vm.deal(_testaddress2, 0);
+      vm.deal(_testaddress3, 0);
+      _contestPrize.Addcomp(1, 0 , 100 ether);
+      vm.deal(address(this), 100 ether);
+      _contestPrize.addbudgeforfreecomp{value : 100 ether}(1);
+      _contestPrize.AwardWithPercentage(payable(_testaddress1), payable(_testaddress2), payable(_testaddress3), 50, 30, 5, 1);
+      assertEq(_testaddress1.balance , 50 ether);
+      assertEq(_testaddress2.balance , 30 ether);
+      assertEq(_testaddress3.balance , 5 ether);
+   }
 
 
 }
